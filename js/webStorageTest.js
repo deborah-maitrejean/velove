ad61a90dfaaa4c7d634cd4d05d9698afa7422726
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-14 17:09:57 +0200

    reformat all code

--- js/webStorageTest.js
+++ js/webStorageTest.js
@@ -1,17 +1,18 @@
 function storageAvailable(type) {
-	try {
-		var storage = window[type],
-		x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch(e) {
-		return false;
-	}
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
 }
+
 if (storageAvailable('sessionStorage')) {
-	console.log('Nous pouvons utiliser sessionStorage');
+    console.log('Nous pouvons utiliser sessionStorage');
 } else {
-	console.log('Malheureusement, sessionStorage n\'est pas disponible');
+    console.log('Malheureusement, sessionStorage n\'est pas disponible');
 }
